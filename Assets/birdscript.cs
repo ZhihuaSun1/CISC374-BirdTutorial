6521627e11787a75f73d15cf208af6462feb886a
ZhihuaSun <sunzhi@udel.edu>
2025-02-23 17:50:24 -0500

    new

---
+++ Assets/birdscript.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class birdscript : MonoBehaviour
+{
+    public Rigidbody2D myRigidbody;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myRigidbody.velocity
+    }
+}
