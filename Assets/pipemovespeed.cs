9f13e3c4588c52885ed2cc8b71bb269adb762926
ZhihuaSun <sunzhi@udel.edu>
2025-02-24 21:22:45 -0500

    1

--- Assets/pipemovespeed.cs
+++ Assets/pipemovespeed.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class pipemovespeed : MonoBehaviour
 {
     public float moveSpeed = 5;
+    public float deadZone = -45;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -14,5 +15,10 @@ public class pipemovespeed : MonoBehaviour
     void Update()
     {
         transform.position = transform.position + (Vector3.left * moveSpeed) * Time.deltaTime;
+        if (transform.position.x < deadZone)
+        {
+            Debug.Log("Pipe Deleted");
+            Destroy(gameObject);
+        }
     }
 }
