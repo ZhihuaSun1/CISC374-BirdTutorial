dd2dd6cc1c44c2c9765222ea2aa0ee82a4e9a6eb
ZhihuaSun <sunzhi@udel.edu>
2025-02-24 21:16:01 -0500

    1

--- Assets/ky.cs
+++ Assets/ky.cs
@@ -5,6 +5,7 @@ public class ky : MonoBehaviour
     public GameObject pipe;
     public float spawnRate = 2;
     private float timer = 0;
+    public float heightOffset = 10;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -23,6 +24,8 @@ public class ky : MonoBehaviour
     }
 
     void spawnPipe(){
-        Instantiate(pipe, transform.position, transform.rotation);
+        float lowestPoint = transform.position.y - heightOffset;
+        float highestPoint = transform.position.y + heightOffset;
+        Instantiate(pipe, new Vector3(transform.position.x, Random.Range(lowestPoint, highestPoint), 0), transform.rotation);
     }
 }
