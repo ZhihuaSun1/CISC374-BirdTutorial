a991fb48fcf7e097dc4e750d4ae9db4993d15c69
ZhihuaSun <sunzhi@udel.edu>
2025-02-27 17:27:52 -0500

    1

--- Assets/logicscript.cs
+++ Assets/logicscript.cs
@@ -6,19 +6,14 @@ public class logicscript : MonoBehaviour
     public int playerScore;
     public Text scoreText;
     public GameObject gameOverScreen;
-    public AudioClip scoreClip;
-    private AudioSource audioSource;
+    public AudioSource dingWAX;
 
     [ContextMenu("Increase Score")]
     public void addScore(int scoreToAdd)
     {
         playerScore = playerScore + scoreToAdd;
         scoreText.text = playerScore.ToString();
-        audioSource = GetComponent<AudioSource>();
-    }
-    public void OnScore()
-    {
-        audioSource.PlayOneShot(scoreClip);
+        dingWAX.Play();
     }
 
     public void restarGame()
