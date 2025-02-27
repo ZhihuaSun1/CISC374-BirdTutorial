10b756bba08ac269592532cb8eea7fd1ed32d5e0
ZhihuaSun <sunzhi@udel.edu>
2025-02-27 18:48:02 -0500

    1

--- Assets/logicscript.cs
+++ Assets/logicscript.cs
@@ -1,19 +1,44 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+
 public class logicscript : MonoBehaviour
 {
     public int playerScore;
-    public Text scoreText;
+    public Text scoreText;           
+    public Text highScoreText;       
     public GameObject gameOverScreen;
     public AudioSource dingWAX;
+
+    private int highScore;
+
+    void Start()
+    {
+        playerScore = 0;
+        scoreText.text = playerScore.ToString();
+        highScore = PlayerPrefs.GetInt("HighScore", 0);
+        if (highScoreText != null)
+        {
+            highScoreText.text = "High Score: " + highScore;
+        }
+    }
 
     [ContextMenu("Increase Score")]
     public void addScore(int scoreToAdd)
     {
-        playerScore = playerScore + scoreToAdd;
+        playerScore += scoreToAdd;
         scoreText.text = playerScore.ToString();
         dingWAX.Play();
+        if (playerScore > highScore)
+        {
+            highScore = playerScore;
+            if (highScoreText != null)
+            {
+                highScoreText.text = "High Score: " + highScore;
+            }
+            PlayerPrefs.SetInt("HighScore", highScore);
+            PlayerPrefs.Save();  
+        }
     }
 
     public void restarGame()
