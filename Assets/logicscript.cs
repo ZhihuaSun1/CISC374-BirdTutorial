735eb2e17967d1ec829ab1c2926e951c8f8c6867
ZhihuaSun <sunzhi@udel.edu>
2025-02-27 16:30:31 -0500

    1

--- Assets/logicscript.cs
+++ Assets/logicscript.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 public class logicscript : MonoBehaviour
 {
@@ -9,5 +10,9 @@ public class logicscript : MonoBehaviour
     {
         playerScore = playerScore + scoreToAdd;
         scoreText.text = playerScore.ToString();
+    }
+
+    public void restarGame(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
