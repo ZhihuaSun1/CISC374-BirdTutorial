ee3db84ee4e9b3982a07f813fc789e8a9feb986d
ZhihuaSun <sunzhi@udel.edu>
2025-02-28 09:14:44 -0500

    1

--- Assets/CloudScript.cs
+++ Assets/CloudScript.cs
@@ -2,27 +2,19 @@ using UnityEngine;
 
 public class CloudSpawner : MonoBehaviour
 {
-    [Header("云朵预制体设置")]
+    [Header("云朵预制体")]
     public GameObject cloudPrefab;
 
-    [Header("生成速率设置")]
-    public float initialSpawnInterval = 1f;
-    public float maxSpawnInterval = 5f;
-
-    public float intervalIncreaseRate = 0.2f;
-
-    public int minCloudsPerBurst = 3;
-    public int maxCloudsPerBurst = 5;
-
-    private float currentSpawnInterval;
+    [Header("生成设置")]
+    public float spawnInterval = 1f;
+    public int maxCloudCount = 6;
+    
     private float spawnTimer = 0f;
-
     private float minX, maxX, minY, maxY;
     private Camera mainCam;
 
     void Start()
     {
-        currentSpawnInterval = initialSpawnInterval;
         mainCam = Camera.main;
         Vector3 bottomLeft = mainCam.ScreenToWorldPoint(new Vector3(0, 0, mainCam.nearClipPlane));
         Vector3 topRight   = mainCam.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, mainCam.nearClipPlane));
@@ -35,23 +27,22 @@ public class CloudSpawner : MonoBehaviour
     void Update()
     {
         spawnTimer += Time.deltaTime;
-        if (spawnTimer >= currentSpawnInterval)
+        if (spawnTimer >= spawnInterval)
         {
-            SpawnClouds();
+            GameObject[] currentClouds = GameObject.FindGameObjectsWithTag("Cloud");
+            if (currentClouds.Length < maxCloudCount)
+            {
+                SpawnCloud();
+            }
             spawnTimer = 0f;
-            currentSpawnInterval = Mathf.Min(currentSpawnInterval + intervalIncreaseRate, maxSpawnInterval);
         }
     }
 
-    void SpawnClouds()
+    void SpawnCloud()
     {
-        int count = Random.Range(minCloudsPerBurst, maxCloudsPerBurst + 1);
-        for (int i = 0; i < count; i++)
-        {
-            float randX = Random.Range(minX, maxX);
-            float randY = Random.Range(minY, maxY);
-            Vector3 spawnPos = new Vector3(randX, randY, 0);
-            Instantiate(cloudPrefab, spawnPos, Quaternion.identity);
-        }
+        float randX = Random.Range(minX, maxX);
+        float randY = Random.Range(minY, maxY);
+        Vector3 spawnPos = new Vector3(randX, randY, 0);
+        Instantiate(cloudPrefab, spawnPos, Quaternion.identity);
     }
 }
